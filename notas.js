b6da30bb0ec588d26c8b0e553802499c1690aafb
Ximena-21 <ximenanzola.21@gmail.com>
2022-07-26 22:33:34 -0500

    notas para git

---
+++ notas.js
@@ -0,0 +1,31 @@
+
+//crear un rama con la historia
+
+    //git checkout -b nombre_de_la_rama
+
+    //git checkout -b feature/mainSection
+
+
+//subir esa rama a github
+
+    //git push origin/upstream nombre_de_la_rama
+
+    //git push origin feature/mainSection -> stefany
+
+    //git push upstream feature/mainSection -> paula
+
+
+//actualizar una rama local que esta en github
+
+    //estar ubicado en la rama a actualizar (la diferencia entre posicionarse y crearse es el -b)
+
+    //git checkout feature/mainSection
+
+    //git pull origin/upstream feature/mainSection
+
+
+//mergear una rama con otra rama
+
+    //posicionarse en la rama a donde quiero mergear
+
+    // git merge nombreRamaMergeada 
