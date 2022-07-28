05ce6692c6abc25c77238ba75e22ab35642beb3c
Ximena-21 <ximenanzola.21@gmail.com>
2022-07-27 22:51:51 -0400

    responsive layout  of the themes page

---
+++ src/js/gameTheme.js
@@ -0,0 +1,10 @@
+const marvelTheme = document.querySelector('#cardMarvel')
+const PokemonTheme = document.querySelector('#cardPokemon')
+
+marvelTheme.addEventListener('click', () => {
+    location.href = './game.html'
+})
+
+PokemonTheme.addEventListener('click', () => {
+    location.href = './game.html'
+})
