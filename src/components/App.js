94302f49cf51ac6e8f260c5fa4bc0f7b947c6c3b
Ximena-21 <ximenanzola.21@gmail.com>
2022-07-29 01:11:18 -0400

    function renderCards

--- src/components/App.js
+++ src/components/App.js
@@ -14,13 +14,81 @@
 //   .catch(console.error);
 //
 
+import pokemon from '../data/pokemon/pokemon.js';
+
+
+
+/* Duplicar las cartas */
+let dataPokemon = pokemon.items;
+console.log(dataPokemon)
+// .concat para duplicar la carta
+dataPokemon = dataPokemon.concat(dataPokemon);
+console.log(dataPokemon)
+
+
+// const shuffle = (allCards) => {
+//   //Se declara la función shuffle para barajar las tarjetas usando el algoritmo de Fisher - Yates
+//   let i = allCards.length - 1;
+//   let temp = 0;
+//   let randomIndex = 0;
+//   while (i >= 0) {
+//     randomIndex = Math.floor(Math.random() * (i + 1));
+//     temp = allCards[randomIndex];
+//     allCards[randomIndex] = allCards[i];
+//     allCards[i] = temp;
+//     i--;
+//   }
+// };
+
+//Proceso de ver las cartas aleatoriamente
+// function fisherShuffle(arr){
+
+//   // const cards = dataPokemon
+
+//   for(let i =arr.length-1 ; i>0 ;i--){
+//       let j = Math.floor( Math.random() * (i + 1) ); //random index
+//       [arr[i],arr[j]]=[arr[j],arr[i]]; // swap
+//   }
+// }
+
+// fisherShuffle(dataPokemon)
+
+// console.log(dataPokemon)
+
+function renderCard(){
+   
+  // mixCard()
+
+  const containerCards = document.querySelector('.game_containerCards')
+
+  dataPokemon.forEach((person)=>{
+    
+    // console.log(person.image)
+    const cardBox = document.createElement('div')
+    cardBox.className = 'game_cardBox'
+
+    const contentCard = `
+      <img class='game_cardBoxImg'>${person.image}</img>
+    `
+    cardBox.innerHTML = contentCard
+    containerCards.appendChild(cardBox)
+  })
+  
+}
+
+
 const App = () => {
-  const el = document.createElement('div');
+  
+  renderCard()
 
-  el.className = 'App';
-  el.textContent = 'Hola mundo!';
+  // const el = document.createElement('div');
 
-  return el;
+  // el.className = 'App';
+  // el.textContent = 'Hola mundo!';
+
+  // return el;
 };
 
 export default App;
+
+
--- src/js/game.js
+++ src/js/game.js
@@ -1,9 +1,3 @@
-import App from './components/App.js';
+import App from '../components/App.js';
 
 document.getElementById('root').appendChild(App());
-
-const button = document.querySelector('button')
-
-button.addEventListener('click', () => {
-    location.href = '../pages/gameTheme.html'
-})
